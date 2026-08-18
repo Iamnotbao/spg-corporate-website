225c0a18988e64f4485fe1049803c0d132555fc6
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2026-08-18 08:32:14 +0700

    fix: add deep link routes for news and careers

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom';
 import Admin from './Admin.jsx';
-import PublicApp from './PublicApp.jsx';
-export default function App(){return window.location.pathname.startsWith('/admin')?<Admin/>:<PublicApp/>;}
+import PublicApp, { CareerDetail, NewsDetail } from './PublicApp.jsx';
+export default function App() { return <Routes><Route path="/admin/*" element={<Admin />} /><Route path="/news/:id" element={<NewsDetail />} /><Route path="/careers/:id" element={<CareerDetail />} /><Route path="*" element={<PublicApp />} /></Routes>; }
