6f217289df8d63a0d9869628b18923516e163dda
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2026-08-22 14:17:50 +0700

    feat(ui): load product polish styles

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App.jsx';
 import './styles/global.css';
+import './styles/product-polish.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
