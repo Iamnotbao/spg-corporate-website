5a9917c8cdf3e94ad544d6b7684655db7eea2fd3
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2026-08-27 11:48:02 +0700

    feat(admin): expose move-to-trash route

--- back-end/src/routes/admin-trash.routes.js
+++ back-end/src/routes/admin-trash.routes.js
@@ -7,6 +7,7 @@ const router = Router();
 
 router.use(auth, requireAdmin);
 router.get("/", asyncHandler(trashController.listTrash));
+router.post("/:type/:id", asyncHandler(trashController.moveToTrash));
 router.post("/:type/:id/restore", asyncHandler(trashController.restoreTrash));
 router.delete("/:type/:id", asyncHandler(trashController.purgeTrash));
 router.delete("/", asyncHandler(trashController.emptyTrash));
