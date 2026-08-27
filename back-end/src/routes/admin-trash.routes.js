3d0320ca4c9321476504766bbb7de9cbcab6b0ae
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2026-08-27 11:45:54 +0700

    feat(admin): add trash routes

---
+++ back-end/src/routes/admin-trash.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { auth, requireAdmin } from "../middleware/auth.js";
+import * as trashController from "../features/trash/trash.controller.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const router = Router();
+
+router.use(auth, requireAdmin);
+router.get("/", asyncHandler(trashController.listTrash));
+router.post("/:type/:id/restore", asyncHandler(trashController.restoreTrash));
+router.delete("/:type/:id", asyncHandler(trashController.purgeTrash));
+router.delete("/", asyncHandler(trashController.emptyTrash));
+
+export default router;
